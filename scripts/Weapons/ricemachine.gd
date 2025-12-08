2b7bd41462e44d7d0c359cb986f8cda585174914
Quynh Tran <87471005+quinmeh@users.noreply.github.com>
2025-12-07 20:25:53 -0800

    finished gun animation

--- scripts/Weapons/ricemachine.gd
+++ scripts/Weapons/ricemachine.gd
@@ -1,10 +1,67 @@
+#extends WeaponBase
+#
+#@export var bullet_scene: PackedScene
+#@onready var weapon_pivot: Marker2D = $WeaponPivot
+#@onready var shooting_point: Marker2D = $WeaponPivot/Gun/ShootingPoint
+#@onready var player = get_tree().current_scene.get_node("Player")
+#@onready var rice_gun_sound =  load("res://assets/sound/Mom/RicechineGun.wav") as AudioStream
+#
+#
+#
+#func _ready():
+	#weapon_type = "Rice Machine"
+	#damage = 10.0
+	#cooldown = 0.1  # Fast fire rate
+#
+#
+#func _process(delta):
+	## Call parent to handle cooldown
+	#super._process(delta)
+#
+	## Rotate the weapon pivot to face mouse - simple pivot
+	#if weapon_pivot != null:
+		#var mouse_pos = get_global_mouse_position()
+		#var angle_to_mouse = global_position.angle_to_point(mouse_pos)
+		#weapon_pivot.rotation = angle_to_mouse
+#
+## Override attack method from WeaponBase
+#func attack() -> bool:
+	#if not super.attack():
+		#return false
+#
+	#_shoot_bullet()
+	#return true
+#
+#func _shoot_bullet() -> void:
+	#if bullet_scene == null:
+		#print("ERROR: bullet_scene is null for Rice Machine!")
+		#return
+#
+	#var bullet = bullet_scene.instantiate()
+	#get_tree().current_scene.add_child(bullet)
+#
+	## Use shooting point if available
+	#if shooting_point != null:
+		#bullet.global_position = shooting_point.global_position
+	#else:
+		#print("ERROR: no shooting point")
+#
+#
+	## Direction based on weapon rotation
+	#var mouse_pos = get_global_mouse_position()
+	#bullet.direction = (mouse_pos - player.global_position).normalized()
+
+
 extends WeaponBase
 
 @export var bullet_scene: PackedScene
 @onready var weapon_pivot: Marker2D = $WeaponPivot
 @onready var shooting_point: Marker2D = $WeaponPivot/Gun/ShootingPoint
 @onready var player = get_tree().current_scene.get_node("Player")
-@onready var rice_gun_sound =  load("res://assets/sound/Mom/RicechineGun.wav") as AudioStream
+@onready var rice_gun_sound = load("res://assets/sound/Mom/RicechineGun.wav") as AudioStream
+
+# NEW: AnimationPlayer for the gun firing animation
+@onready var gun_anim_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	weapon_type = "Rice Machine"
@@ -44,7 +101,10 @@ func _shoot_bullet() -> void:
 	else:
 		print("ERROR: no shooting point")
 
-
 	# Direction based on weapon rotation
 	var mouse_pos = get_global_mouse_position()
 	bullet.direction = (mouse_pos - player.global_position).normalized()
+
+	# 🔥 Play firing animation
+	if gun_anim_player:
+		gun_anim_player.play("gun_animation")
